7ed5e4631a2a5742c124cc47f4ef997304f98d79
aryanparle <134794976+aryanparle@users.noreply.github.com>
2024-04-25 22:09:14 -0700

    Add files via upload

--- Lesson 7.py
+++ Lesson 7.py
@@ -11,9 +11,22 @@ print("The factorial value of",n,"=",fact(n))'''
 #FIBONACCI
 def fibona(n):
     '''The following function shows the recursive nature of the function.'''
-    if(n<=1):
+    '''if(n<=1):
         return (n)
     else:
         return(fibona(n-1)+fibona(n-2))
 n=int(input("Enter an Integer : "))
-print("The fibonacci value of",n,"=",fibona(n))
+print("The fibonacci value of",n,"=",fibona(n))'''
+
+#GREATEST COMMON DIVISOR
+def gcd(a,b):
+    '''The following function shows the recursive nature of the function.'''
+    r=a%b
+    while(r>0):
+        a=b
+        b=r
+        r=a%b
+    return(b)
+a=int(input("Enter the value of dividend : "))
+b=int(input("Enter the value of divisor : "))
+print("Remainder of dividend",a,"and divisor",b,"=",gcd(a,b))
